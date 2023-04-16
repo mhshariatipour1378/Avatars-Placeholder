48e738154a2bbdb37f7582f639f2912925c76b96
mhshariatipour1378 <mohammadhosein.sh1378@gmail.com>
2023-04-17 01:43:41 +0430

    fix api request

--- back-end/app/http/controller/apiController/index.ts
+++ back-end/app/http/controller/apiController/index.ts
@@ -2,32 +2,45 @@ import { Request, Response, NextFunction } from 'express';
 
 class postController{
 
+    publicNameAvatar: string = process.env.AVATAR_PUBLIC_NAME ? process.env.AVATAR_PUBLIC_NAME : "";
+    foramtFile:  string = process.env.AVATAR_FORAMT_FILE ? process.env.AVATAR_FORAMT_FILE : "";
+
     index = (req: Request, res: Response, next: NextFunction) => {
 
         const startIndex: number = process.env.IMG_START_INDEX ? parseInt(process.env.IMG_START_INDEX ) : 0;
         const endIndex: number = process.env.IMG_END_INDEX ? parseInt(process.env.IMG_END_INDEX ) : 0;
         
-        const startIndexBoy: number = process.env.IMG_START_INDEX ? parseInt(process.env.IMG_START_INDEX ) : 0;
-        const endIndexBoy: number = process.env.IMG_END_INDEX ? parseInt(process.env.IMG_END_INDEX ) : 0;
+        const startIndexBoy: number = process.env.IMG_BOY_START_INDEX ? parseInt(process.env.IMG_BOY_START_INDEX) : 0;
+        const endIndexBoy: number = process.env.IMG_BOY_END_INDEX ? parseInt(process.env.IMG_BOY_END_INDEX) : 0;
         
-        const startIndexGirl: number = process.env.IMG_BOY_START_INDEX ? parseInt(process.env.IMG_BOY_START_INDEX) : 0;
-        const endIndexGirl: number = process.env.IMG_BOY_END_INDEX ? parseInt(process.env.IMG_BOY_END_INDEX) : 0;
+        const startIndexGirl: number = process.env.IMG_GIRL_START_INDEX ? parseInt(process.env.IMG_GIRL_START_INDEX) : 0;
+        const endIndexGirl: number = process.env.IMG_GIRL_END_INDEX ? parseInt(process.env.IMG_GIRL_END_INDEX) : 0;
+
+        let allAvatars: string[] = [];
+        let boyAvatars: string[] = [];
+        let girlAvatars: string[] = [];
+
+        for(let i = startIndex; i <= endIndex; i++){
+            const path: string = `/public/${i}`;
+            allAvatars.push(path);
+        }
+
+        for(let i = startIndexBoy; i <= endIndexBoy; i++){
+            const path: string = `/public/${i}`;
+            boyAvatars.push(path);
+        }
+
+        for(let i = startIndexGirl; i <= endIndexGirl; i++){
+            const path: string = `/public/${i}`;
+            girlAvatars.push(path);
+        }
 
         res.
         status(200).
         json({
-            all:{
-                strat: startIndex,
-                end: endIndex
-            },
-            boy:{
-                strat: startIndexBoy,
-                end: endIndexBoy
-            },
-            girl:{
-                strat: startIndexGirl,
-                end: endIndexGirl
-            },
+            all: allAvatars,
+            boy: boyAvatars,
+            girl: girlAvatars,
         });
 
     }
--- back-end/dist/app/http/controller/apiController/index.js
+++ back-end/dist/app/http/controller/apiController/index.js
@@ -2,28 +2,36 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class postController {
     constructor() {
+        this.publicNameAvatar = process.env.AVATAR_PUBLIC_NAME ? process.env.AVATAR_PUBLIC_NAME : "";
+        this.foramtFile = process.env.AVATAR_FORAMT_FILE ? process.env.AVATAR_FORAMT_FILE : "";
         this.index = (req, res, next) => {
             const startIndex = process.env.IMG_START_INDEX ? parseInt(process.env.IMG_START_INDEX) : 0;
             const endIndex = process.env.IMG_END_INDEX ? parseInt(process.env.IMG_END_INDEX) : 0;
-            const startIndexBoy = process.env.IMG_START_INDEX ? parseInt(process.env.IMG_START_INDEX) : 0;
-            const endIndexBoy = process.env.IMG_END_INDEX ? parseInt(process.env.IMG_END_INDEX) : 0;
-            const startIndexGirl = process.env.IMG_BOY_START_INDEX ? parseInt(process.env.IMG_BOY_START_INDEX) : 0;
-            const endIndexGirl = process.env.IMG_BOY_END_INDEX ? parseInt(process.env.IMG_BOY_END_INDEX) : 0;
+            const startIndexBoy = process.env.IMG_BOY_START_INDEX ? parseInt(process.env.IMG_BOY_START_INDEX) : 0;
+            const endIndexBoy = process.env.IMG_BOY_END_INDEX ? parseInt(process.env.IMG_BOY_END_INDEX) : 0;
+            const startIndexGirl = process.env.IMG_GIRL_START_INDEX ? parseInt(process.env.IMG_GIRL_START_INDEX) : 0;
+            const endIndexGirl = process.env.IMG_GIRL_END_INDEX ? parseInt(process.env.IMG_GIRL_END_INDEX) : 0;
+            let allAvatars = [];
+            let boyAvatars = [];
+            let girlAvatars = [];
+            for (let i = startIndex; i <= endIndex; i++) {
+                const path = `/public/${i}`;
+                allAvatars.push(path);
+            }
+            for (let i = startIndexBoy; i <= endIndexBoy; i++) {
+                const path = `/public/${i}`;
+                boyAvatars.push(path);
+            }
+            for (let i = startIndexGirl; i <= endIndexGirl; i++) {
+                const path = `/public/${i}`;
+                girlAvatars.push(path);
+            }
             res.
                 status(200).
                 json({
-                all: {
-                    strat: startIndex,
-                    end: endIndex
-                },
-                boy: {
-                    strat: startIndexBoy,
-                    end: endIndexBoy
-                },
-                girl: {
-                    strat: startIndexGirl,
-                    end: endIndexGirl
-                },
+                all: allAvatars,
+                boy: boyAvatars,
+                girl: girlAvatars,
             });
         };
     }
